71514615c44576f6d606166af00c8baec9c85d71
avashist1998 <avashist98@tamu.edu>
2021-12-15 19:58:31 -0600

    feat: example api call

--- ui/src/pages/PlayPage.tsx
+++ ui/src/pages/PlayPage.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import NavBar from '../components/navbar';
 
+async function test(){
+    fetch("http://127.0.0.1:8000").then(Response => {
+        Response.json().then(Json => {
+            console.log(Json.message)
+        });
+    })  
+}
+
+function undo(){
+    console.log("clear the screen")
+
+}
+
 function PlayPage() {
     return (
+
         <div className="App">
             {/* <nav className='nav-bar'></nav> */}
             <NavBar/>
             <header className="Number Practice">
             </header>
+
+            PlayPage
+            <button onClick={test}>Submit</button>
+            <button onClick={undo}>Clear</button>
+
         </div>
     )
 }
