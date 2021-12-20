572e382bb4c9fe79d89d1da4076399d49ffbd916
hluong0912 <huy.luong0912@gmail.com>
2021-12-19 19:52:19 -0600

    merge: Play.tsx, changed Navbar.tsx to capitalize filename

---
+++ ui/src/components/DrawingCanvas.tsx
@@ -0,0 +1,66 @@
+import React, 
+    {
+        useRef, useLayoutEffect, MouseEvent,
+        Dispatch, SetStateAction 
+    } from 'react';
+// import './PlayPage.css'
+
+//make interface for passing props
+interface IProps {
+  setImageURI?: Dispatch<SetStateAction<string>>;
+}
+
+
+const DrawingCanvas:React.FC<IProps> = ({setImageURI}) => {
+      
+    const refCanvas = useRef<HTMLCanvasElement>(null) 
+    var contextRefCanvas:any = null
+    var boundCanvas:DOMRect
+    var isPainting:boolean = false
+
+    useLayoutEffect(() => {
+        console.log(refCanvas); // { current: <h1_object> }
+        contextRefCanvas = refCanvas.current!.getContext('2d');
+        boundCanvas = refCanvas.current!.getBoundingClientRect()
+    })
+
+    const startPosition = (e:MouseEvent<HTMLCanvasElement>) =>{
+        isPainting = true
+        // console.log(refCanvas.current?.getBoundingClientRect())
+        // console.log({X:e.pageX, Y:e.pageY})
+        // console.log({X:e.clientX, Y:e.clientY})
+        draw(e) // this for event of 1 dot only
+    }
+    const stopPosition = () =>{
+        isPainting = false
+        contextRefCanvas!.beginPath()
+        setImageURI!(refCanvas.current!.toDataURL())
+        // console.log(refCanvas.current!.toDataURL())
+        // console.log("toDataURL:",contextRefCanvas!.toDataURL())
+    }
+
+
+    const draw = (e:MouseEvent<HTMLCanvasElement>)=> {
+        if (!isPainting) return;
+        let offsetX = boundCanvas!.x
+        let offsetY = boundCanvas!.y
+        contextRefCanvas!.lineWidth = 5
+        contextRefCanvas!.lineCap   = "round"
+        contextRefCanvas!.lineTo(e.pageX - offsetX, e.pageY - offsetY) //
+        contextRefCanvas!.stroke() // this will draw the line
+    }
+    
+    return (
+            <canvas id="myCanvas" 
+            ref={refCanvas}
+            onMouseUp={stopPosition}
+            onMouseDown={startPosition}
+            onMouseMove={draw}
+            width="500px" height="500px"></canvas>
+    )
+};
+
+export default DrawingCanvas;
+
+//https://github.com/rleija703/react-examples/blob/master/useref-with-typescript/src/App.tsx
+//https://linguinecode.com/post/how-to-use-react-useref-with-typescript
--- ui/src/pages/Play.tsx
+++ ui/src/pages/Play.tsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
+import NavBar from '../components/navbar';
+import DrawingCanvas from '../components/DrawingCanvas';
+import './Play.css'
 
-function PlayPage() {
-
+const PlayPage = () => {   
+    const [canvasURI, setCanvasURI] = useState("")   
     const [number, SetNumber] = useState(0);
     const [score, SetScore] = useState(0.99);
-
-
-    async function test(){
+        
+    const test = () => {
         let data = {"image":"23"}
         fetch("http://127.0.0.1:8000/predict/?" + new URLSearchParams(data)).then(Response => {
             Response.json().then(Json => {
@@ -16,29 +18,30 @@ function PlayPage() {
             });
         })  
     }
-    
-    function undo(){
+
+    const undo = () => {
         console.log("clear the screen")
         SetNumber(0)
         SetScore(0.99)
     }
 
     return (
-
         <div className="Play">
             <div className='container'>
-
+                <DrawingCanvas setImageURI={setCanvasURI}/>
+                <img className="signature-thumbnail" width="250px" 
+                src ={canvasURI} 
+                alt="Signature thumbnail"></img>
                 <button className="submitButton" onClick={test}>Submit</button>
                 <button className="submitButton" onClick={undo}>Clear</button>
                 <p>Your number is : {number}</p>
                 <p>The score given by model {score}</p>
-
             </div>
-
-
-
         </div>
     )
-}
+};
 
 export default PlayPage;
+
+//https://github.com/rleija703/react-examples/blob/master/useref-with-typescript/src/App.tsx
+//https://linguinecode.com/post/how-to-use-react-useref-with-typescript
