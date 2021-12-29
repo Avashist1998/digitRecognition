5538a298ecaa08a0a1f03ea66f37ffc390340ef0
avashist <abhay.vashist1998@gmail.com>
2021-12-29 14:58:09 -0600

    feat: apiCall complete

--- ui/src/components/SubPlayPage.tsx
+++ ui/src/components/SubPlayPage.tsx
@@ -1,27 +1,58 @@
+import { METHODS } from "http";
 import React, { useState } from "react";
 import DrawingCanvas from "./DrawingCanvas";
 
-const SubPlayPage = ({number_truth = "0"}) => {   
+const SubPlayPage = ({number_truth = "0"}) => {
+    
+    const url = "http://192.168.1.65:8000/predict"
+    const [imageData, setImageData] = useState<Blob | null>(null)
     const [canvasURI, setCanvasURI] = useState("")  
     const [number_result, SetNumberResult] = useState(0);
     const [score_result, SetScoreResult] = useState(0.99);
 
-    const test = () => {
-        let data = {"image":"23"}
-        fetch("http://127.0.0.1:8000/predict/?" + new URLSearchParams(data)).then(Response => {
-            Response.json().then(Json => {
-                console.log(Json.data);
-                SetNumberResult(Json.data.classification)
-                SetScoreResult(Json.data.score)
-            });
-        })  
+
+    const getResults = () => {
+        if (imageData != null){
+            const fd = new FormData()
+            fd.append('file', imageData, "file.png")
+            fetch(url, {
+                method: "POST",
+                body: fd }).then(Response => { 
+                    Response.json().then(Json => {
+                        console.log(Json.data);
+                        SetNumberResult(Json.data.classification)
+                        SetScoreResult(Json.data.score)
+                    });
+                })
+                .catch(err => console.error(err))
+        }
+        }
+        
+        
+
+
+    const submit = () => {
+        
+        var canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+        var image = canvas.toDataURL()
+        // setImageData(image)
+        canvas.toBlob(function(blob){
+            if (blob != null) {
+                setImageData(blob)
+                getResults()
+            }
+            
+         })
+
     }
     
     const undo = () => {
         console.log("clear the screen")
-        console.log({canvasURI})
-        setCanvasURI!("/")
-        console.log({canvasURI})
+        var canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+        var context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Need to hide the Set Number and Score
         SetNumberResult(0)
         SetScoreResult(0.99)
     }
@@ -40,7 +71,7 @@ const SubPlayPage = ({number_truth = "0"}) => {
 
                 <div className="row">
                     <div className="col-md-2 col-lg-2 offset-md-4 offset-lg-4">
-                        <button className="btn btn-success" onClick={test}>Submit</button>
+                        <button className="btn btn-success" onClick={submit}>Submit</button>
                     </div>
                     <div className="col-md-2 col-lg-2">
                         <button className="btn btn-danger" onClick={undo}>Clear</button>
